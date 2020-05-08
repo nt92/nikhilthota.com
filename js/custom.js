729b1bc290461a33bb5936d75dd0302a9c6dcbb2
Nikhil Thota <nikhilthota@fb.com>
2020-05-08 16:08:13 -0400

    updated google analytics

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,3 @@
-// Google Analytics
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', '{{ .Site.GoogleAnalytics }}', 'auto');
-
-// Extras (https://github.com/googleanalytics/autotrack)
-ga('require', 'outboundLinkTracker');
-ga('require', 'mediaQueryTracker');
-ga('require', 'impressionTracker');
-ga('require', 'maxScrollTracker');
-ga('require', 'pageVisibilityTracker');
-
-ga('send', 'pageview');
-
 // Prevent copying images
 document.addEventListener('contextmenu', event => {
     if (event.target.tagName === 'IMG') {
